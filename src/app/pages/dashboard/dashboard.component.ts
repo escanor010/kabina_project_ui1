4a480ac06238403f3869d987556e8bc9d30cc834
minhquanvu97 <1512442@student.hcmus.edu.vn>
2019-09-07 21:40:36 +0700

    Automatically update data every 4hours Admin report features

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
 import { Label } from 'ng2-charts';
 import { max } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
 import { AdminService } from '../../services/admin.service';
 
 @Component({
@@ -29,6 +31,111 @@ export class DashboardComponent implements OnInit {
   constructor(private adminService: AdminService) { }
 
   ngOnInit() {
+    this.generateData();
+    Observable
+    .interval(14400*1000)
+    .timeInterval()
+    .flatMap(() => this.adminService.generateReportShelves())
+    .subscribe((data : any) => {
+        let floorReport = data.FloorReport;
+        let businessUnitReport = data.BusinessUnitReport;
+        let lablesFloor: Array<string>=[];
+        let floorAvai: Array<number>=[];
+        let floorBook: Array<number>=[];
+        let buAvai: Array<number>=[];
+        let buBook: Array<number>=[];
+        let lablesBusinessUnit: Array<string>=[];
+
+        var floorDataSets : any[] = [];
+        var buDataSets : any[] = [];
+        var tBu : any[] = [];
+        var tFloor : any[] = [];
+
+        for (let entryf of floorReport) {
+            var inforf = {
+              name: 'Floor ' + entryf.floorNumber,
+              availables: entryf.available,
+              booked: entryf.booked,
+              percentage: entryf.booked*100/(entryf.booked+entryf.available)  
+            };
+            tFloor.push(inforf);
+            lablesFloor.push('Floor '+ entryf.floorNumber);
+            floorBook.push(entryf.booked);
+            floorAvai.push(entryf.available);
+        }
+        this.tFloor = tFloor;
+        floorDataSets[0] = {
+            data : floorBook,
+            label: 'Booked'
+        }
+        floorDataSets[1] = {
+            data : floorAvai,
+            label : 'Available'
+        }
+
+        for (let entryb of businessUnitReport) {
+            var inforb = {
+              name: entryb.name,
+              availables: entryb.available,
+              booked: entryb.booked,
+              percentage: entryb.booked*100/(entryb.booked+entryb.available)  
+            };
+            tBu.push(inforb);
+            lablesBusinessUnit.push(entryb.name);
+            buBook.push(entryb.booked);
+            buAvai.push(entryb.available);
+        }
+        this.tBu = tBu;
+        buDataSets[0] = { 
+            data : buBook,
+            label : 'Booked'
+        }
+        buDataSets[1] = { 
+            data : buAvai,
+            label : 'Available'
+        }
+
+        var maxBu = Math.max(...buAvai) +  Math.max(...buBook); 
+        var maxFl = Math.max(...floorAvai) +  Math.max(...floorBook); 
+
+        var ChartOptionsBU = {
+          responsive: true,
+          scales: { xAxes: [{}], yAxes: [{ticks:{max:maxBu,userCallback: function(label, index, labels) {
+            if (Math.floor(label) === label) {
+                return label;
+            }
+        }}}] },
+          plugins: {
+            datalabels: {
+              anchor: 'end',
+              align: 'end',
+            }
+          }
+        };
+        this.barChartOptionsBU = ChartOptionsBU;
+        var ChartOptionsFloor = {
+          responsive: true,
+          scales: { xAxes: [{}], yAxes: [{ticks:{max:maxFl,userCallback: function(label, index, labels) {
+            if (Math.floor(label) === label) {
+                return label;
+            }
+        }}}] },
+          plugins: {
+            datalabels: {
+              anchor: 'end',
+              align: 'end',
+            }
+          }
+        };
+        this.barChartOptionsFloor = ChartOptionsFloor;
+        this.barChartLabelsBU = lablesBusinessUnit;
+        this.barChartDataBU = buDataSets;
+        this.barChartLabelsFloor = lablesFloor;
+        this.barChartDataFloor = floorDataSets;
+    });
+  }
+
+  generateData(){
     this.adminService.generateReportShelves().subscribe((data : any) => { 
       //let data = {"FloorReport":[{"floorNumber":1,"availableShelves":[],"available":0,"bookedShelves":[{"id":1,"floor":1,"positionNumber":10,"status":"Booked","businessUnit":{"id":1,"name":"Insurance"}}],"booked":1},{"floorNumber":2,"availableShelves":[{"id":2,"floor":2,"positionNumber":20,"status":"Available","businessUnit":{"id":1,"name":"Insurance"}}],"available":1,"bookedShelves":[],"booked":0},{"floorNumber":3,"availableShelves":[{"id":3,"floor":3,"positionNumber":30,"status":"Available","businessUnit":{"id":2,"name":"Admin"}}],"available":1,"bookedShelves":[],"booked":0},{"floorNumber":4,"availableShelves":[{"id":4,"floor":4,"positionNumber":40,"status":"Available","businessUnit":{"id":2,"name":"Admin"}}],"available":1,"bookedShelves":[],"booked":0},{"floorNumber":5,"availableShelves":[],"available":0,"bookedShelves":[{"id":5,"floor":5,"positionNumber":50,"status":"Booked","businessUnit":{"id":3,"name":"Human Resource"}}],"booked":1},{"floorNumber":6,"availableShelves":[],"available":0,"bookedShelves":[{"id":6,"floor":6,"positionNumber":60,"status":"Booked","businessUnit":{"id":3,"name":"Human Resource"}}],"booked":1}],"BusinessUnitReport":[{"id":2,"name":"Admin","availableShelves":[{"id":3,"floor":3,"positionNumber":30,"status":"Available","businessUnit":{"id":2,"name":"Admin"}},{"id":4,"floor":4,"positionNumber":40,"status":"Available","businessUnit":{"id":2,"name":"Admin"}}],"available":2,"bookedShelves":[],"booked":0},{"id":3,"name":"Human Resource","availableShelves":[],"available":0,"bookedShelves":[{"id":5,"floor":5,"positionNumber":50,"status":"Booked","businessUnit":{"id":3,"name":"Human Resource"}},{"id":6,"floor":6,"positionNumber":60,"status":"Booked","businessUnit":{"id":3,"name":"Human Resource"}}],"booked":2},{"id":1,"name":"Insurance","availableShelves":[{"id":2,"floor":2,"positionNumber":20,"status":"Available","businessUnit":{"id":1,"name":"Insurance"}}],"available":1,"bookedShelves":[{"id":1,"floor":1,"positionNumber":10,"status":"Booked","businessUnit":{"id":1,"name":"Insurance"}}],"booked":1}]};
       let floorReport = data.FloorReport;
